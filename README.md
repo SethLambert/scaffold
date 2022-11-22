09bd11ff40dceaf971d37fe35a21fa9867f6207c
Lambert <SL5507@mh.mercyhealthcare.org>
2022-11-22 15:21:37 -0600

    finished calculator module

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic python project Scaffolding
+
+Some text in the document...
+
+- bullet A
+- bullet B
+- bullet C
+
+```python
+some_number: int = 5
+```
+
+> I really want to highlight this text 
+> ... because it's important
---
+++ src/calculator.py
@@ -0,0 +1,25 @@
+"""Basic calculator module."""
+
+def addIntegers(num1: int, num2: int) -> int:
+    """Adds two numbers.
+    
+    Args:
+        num1 (int) - first number to add
+        num2 (int) - second number to add (to the first)
+
+    Returns:
+        int - num1 added to num 2
+    """
+    return num1 + num2
+
+def subtractIntegers(num1: int, num2: int) -> int:
+    """Subtracts num2 from num1.
+    
+    Args:
+        num1 (int) - the number to subtract the value from 
+        num2 (int) - the num to subtract (from num1)
+
+    Returns:
+        int - num1 minus num 2
+    """
+    return num1 - num2    
